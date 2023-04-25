0149dd77e3eb915bc0fe746781997d9c56b45e3c
adrian <adrianirwin28@gmail.com>
2023-04-25 21:04:40 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Socials from "./components/Socials";
 export default function App() {
     return (
         <div className="App flex flex-col min-h-screen justify-center items-center text-white text-center">
-            <h1 className="text-5xl sm:text-5xl text-white">adrian</h1>
+            <h1 className="text-5xl sm:text-5xl text-white">adrianirwin</h1>
 
             <Socials />
         </div>

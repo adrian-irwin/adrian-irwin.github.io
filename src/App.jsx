54f1eb9304dfe096a4b45b5f5675f1ea6b896cff
adrian <adrianirwin28@gmail.com>
2023-05-29 22:24:28 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Links from "./components/Links";
 export default function App() {
     return (
         <div className="App flex flex-col min-h-screen justify-center items-center text-white text-center">
-            <h1 className="text-5xl text-white">adrianirwin</h1>
+            <h1 className="text-5xl text-white">adrian</h1>
             <Links />
         </div>
     );

85c9a0d79703de8fcd910576f9027b425b222275
adrian <adrianirwin28@gmail.com>
2023-09-26 18:58:55 +0100

    Add routing + route to cv

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import CV from "./pages/CV";
 import "./index.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <Routes>
+                <Route path="/">
+                    <Route index element={<App />} />
+                    <Route path="cv" element={<CV />} />
+                </Route>
+            </Routes>
+        </Router>
     </React.StrictMode>
 );

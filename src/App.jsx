48ffd3f8dfbe7d7c8e0959d98b70cc305df52fb9
adrian <adrianirwin28@gmail.com>
2023-10-02 14:50:12 +0100

    Update App.jsx
    remove CV link on main page

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,6 @@ export default function App() {
         <div className="App flex flex-col min-h-screen justify-center items-center text-white text-center">
             <h1 className="text-5xl text-white">adrian</h1>
             <Links />
-            <Link to="/cv" className="text-2xl text-white">
-                cv
-            </Link>
         </div>
     );
 }

d960faed363cd661c9e0d5839ef70620d87beb04
airwin <adrianirwin28@gmail.com>
2023-10-02 19:23:02 +0100

    Remove CV and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Links from "./components/Links";
-import { Link } from "react-router-dom";
 
 export default function App() {
     return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import CV from "./pages/CV";
 import "./index.css";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <Router>
-            <Routes>
-                <Route path="/">
-                    <Route index element={<App />} />
-                    <Route path="cv" element={<CV />} />
-                </Route>
-            </Routes>
-        </Router>
+        <App />
     </React.StrictMode>
 );
